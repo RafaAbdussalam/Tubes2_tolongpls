14e6e40b7907ea99a76364b3097e9b4306ecf83c
Nazih Najmudin <163629526+nazihstei@users.noreply.github.com>
2025-05-07 22:07:36 +0700

    feat: setup frontend

--- src/frontend/lib/api.ts
+++
@@ -1,6 +0,0 @@
-export async function getRecipeTree(element: string) {
-  const response = await fetch(
-    `http://localhost:8080/api/tree?element=${element}`
-  );
-  return await response.json();
-}
